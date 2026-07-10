7c0dcffe61f7e0aaa6377b4f672e4cff0d42eeec
esttomasvega <est.tomasvega@highlandsquito.edu.ec>
2026-07-10 17:00:31 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
-// =========================
+// =====================================
 // T-M
 // Archivo principal de JavaScript
-// =========================
-
-console.log("T-M iniciado correctamente");
+// =====================================
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("La página se cargó correctamente.");
+
+    console.log("T-M iniciado correctamente.");
+
+    console.log("Página cargada correctamente.");
+
 });
